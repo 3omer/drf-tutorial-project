f737381843e2a07f0195423f274b699b43d7ff07
Omar M. Osman <omarosmandev@gmail.com>
2021-06-13 14:31:54 +0200

    add: pagination

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -125,3 +125,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# DEF settings
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
