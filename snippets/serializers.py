4caa88932e630b1ce3eef8654f018094e375bb94
Omar M. Osman <omarosmandev@gmail.com>
2021-06-13 14:27:00 +0200

    add: better api-navigation:  hyperlinks and root api views

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,21 +3,23 @@ from rest_framework import serializers
 from snippets.models import Snippet
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
 
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(
+        view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ['id', 'title', 'code', 'linenos',
-                  'language', 'style', 'owner']
+        fields = ['url', 'id', 'title', 'code', 'linenos',
+                  'language', 'style', 'owner', 'highlight']
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
 
-    snippets = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=Snippet.objects.all())
+    snippets = serializers.HyperlinkedRelatedField(
+        many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'snippets']
+        fields = ['url', 'id', 'username', 'snippets']
--- snippets/views.py
+++ snippets/views.py
@@ -1,14 +1,29 @@
 from django.contrib.auth.models import User
-from rest_framework import status
-from rest_framework.views import APIView
 from django.http import Http404
-from rest_framework.response import Response
-from rest_framework import mixins
 from rest_framework import generics
+from rest_framework import mixins
 from rest_framework import permissions
+from rest_framework import renderers
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    """
+    Root api view
+    """
+
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 
 class SnippetsList(generics.ListCreateAPIView):
@@ -35,6 +50,19 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    """
+    Retrieve HTML representation of a code snippet
+    """
+
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 class UserList(generics.ListAPIView):
